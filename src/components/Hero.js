3db1f8c4973b81e148c77e5ea923b443e2d2e4cd
Nile Camai <nile.camai@gmail.com>
2020-12-20 02:01:04 -0800

    Tweak mobile support for Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { Row, Col, Jumbotron, Container } from 'react-bootstrap';
+import { isMobile } from 'react-device-detect';
 
 function Hero(props) {
+
+    const titleFont = isMobile ? "display-3 text-center" : "display-1";
+
+    const image =   <Row className="justify-content-center">
+                        { props.imgSrc != null && <img className="n-hero-image" src={props.imgSrc} alt={props.imgSrc} />}
+                    </Row>
 
     return(
         <Jumbotron className="bg-transparent jumbotron-fluid p-0">
             <Container fluid={true}>
-                <Row className="justify-content-center py-5">
+                <Row className="justify-content-center py-3">
                     <Col md={8} sm={12}>
                         <Row className="justify-content-center centered">
                             <Col className="justify-content-center">
-                                { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
+                                { props.title && <h1 className={titleFont}>{props.title}</h1> }
                                 { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
                                 { props.bodyText && <h3 className="lead font-weight-light">{props.bodyText}</h3> }
                             </Col>
-                            <Col>
-                                <Row className="justify-content-center">
-                                    { props.imgSrc != null && <img className="n-hero-image" src={props.imgSrc} alt={props.imgSrc} />}
-                                </Row>
-                            </Col>
+                            { !isMobile &&
+                                <Col>
+                                    { image }
+                                </Col>
+                            }
                         </Row>
                     </Col>
                 </Row>
+                { isMobile && image }
             </Container>
         </Jumbotron>
     );
