c924644358e8e5f37ec7fc145871e6ea57189839
Nile Camai <nile.camai@gmail.com>
2020-12-25 00:04:05 -0800

    Fix hero flexbox for title and image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero(props) {
 
     const titleFont = isMobile ? "display-3 text-center" : "display-1";
 
-    const image =   <Row className="justify-content-center">
+    const image =   <Row className="justify-content-center flex-grow-0">
                         { props.imgSrc != null && <img className="n-hero-image" src={props.imgSrc} alt={props.imgSrc} />}
                     </Row>
 
@@ -15,18 +15,16 @@ function Hero(props) {
             <Container fluid={true}>
                 <Row className="justify-content-center py-3">
                     <Col md={8} sm={12}>
-                        <Row className="justify-content-center centered">
-                            <Col className="justify-content-center">
-                                { props.title && <h1 className={titleFont}>{props.title}</h1> }
-                                { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
-                                { props.bodyText && <h3 className="lead font-weight-light">{props.bodyText}</h3> }
-                            </Col>
-                            { !isMobile &&
-                                <Col>
-                                    { image }
+                        <Container fluid={true}>
+                            <Row className="d-flex justify-content-between">
+                                <Col className="justify-content-center d-flex flex-column p-0">
+                                    { props.title && <h1 className={titleFont}>{props.title}</h1> }
+                                    { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
+                                    { props.bodyText && <h3 className="lead font-weight-light">{props.bodyText}</h3> }
                                 </Col>
-                            }
-                        </Row>
+                                { !isMobile && image }
+                            </Row>
+                        </Container>
                     </Col>
                 </Row>
                 { isMobile && image }
