503fc89c81e5d7d353c08ab35b891271f2d41383
Nile Camai <nile.camai@gmail.com>
2020-12-24 22:10:14 -0800

    Add social icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { SocialIcon } from 'react-social-icons';
+import { isMobile } from 'react-device-detect';
+
+let socialIcons = (
+    <Col className="p-0 n-justify-center justify-content-between" md={3} sm={12}>
+        <span className="d-flex justify-content-center">
+            <SocialIcon className="n-social-icon mx-2" url="https://linkedin.com/in/nile-camai" />
+            <SocialIcon className="n-social-icon mx-2" url="https://github.com/nilecamai" />
+        </span>
+    </Col>)
+
+let leftFooterAlign = "p-0 justify-content-center d-flex flex-column " + ( isMobile ? "text-center" : "text-left");
+let rightFooterAlign = "p-0 justify-content-center d-flex flex-column " + ( isMobile ? "text-center" : "text-right");
 
 function Footer() {
 
     return(
         <footer className="mt-5">
-            <Container fluid={true} className="">
-                <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0" md={3} sm={12}>
+            <Container fluid={true} className="border-top py-3">
+                { isMobile && socialIcons }
+                <Row className="justify-content-between p-3">
+                    <Col className={leftFooterAlign} md={3} sm={12}>
                         © 2020 by Nile Camai
                     </Col>
-                    <Col classname="p-0 d-flex justify-content-end" md={3} sm={12}>
+                        { !isMobile && socialIcons}
+                    <Col className={rightFooterAlign} md={3} sm={12}>
                         Created with love by Nile Camai
                     </Col>
                 </Row>
