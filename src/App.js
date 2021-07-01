f1db1fcf1e2cd75f91fd8d056af61272c75ad886
Nile Camai <nile.camai@gmail.com>
2021-07-01 14:32:06 -0700

    Encapsulate Navbar element as Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 //import { render } from '@testing-library/react';
-import { Container, Navbar, Nav } from 'react-bootstrap/';
-import { HashRouter as HashRouter, Route, Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap/';
+import { HashRouter as HashRouter, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -11,7 +12,6 @@ import ContactPage from './pages/ContactPage';
 
 import data from './data/data';
 import ProjectsPage from './pages/ProjectsPage';
-import logo from './assets/images/logo192.png';
 
 class App extends React.Component {
 
@@ -30,22 +30,8 @@ class App extends React.Component {
     return(
       <HashRouter>
         <Container className="p-0" fluid={true}>
-          <Navbar sticky="top" className="border-bottom" bg="white" expand="lg">
-            <Navbar.Brand>
-              {data.title}
-              <img src={logo} style={{height: "2rem", marginBottom: "0.25rem"}}/>
-            </Navbar.Brand>
-
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto">
-                <Link className="nav-link" to={pages[0].path}>{pages[0].nav}</Link>
-                <Link className="nav-link" to={pages[2].path}>{pages[2].nav}</Link>
-                <Link className="nav-link" to={pages[1].path}>{pages[1].nav}</Link>
-                <Link className="nav-link" to={pages[3].path}>{pages[3].nav}</Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+          
+          <Header pages={pages} />
 
           <Route path={pages[0].path} exact render={() => <HomePage /> } />
           <Route path={pages[1].path} render={() => <AboutPage page={pages[1]} /> } />
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import data from '../data/data';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
+
+import logo from '../assets/images/logo192.png';
+
+function Header(props) {
+
+  const pages = props.pages
+
+  const [expanded, setExpanded] = useState(false)
+
+    return(
+        <Navbar sticky="top" className="border-bottom" bg="white" expand="lg" expanded={expanded}>
+            <Navbar.Brand>
+              <Link to={pages[0].path} className="nav-home-link">
+                { !isMobile && data.title }
+                <img src={logo} className="nav-icon"/>
+              </Link>
+            </Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" onClick={() => setExpanded(expanded ? false : "expanded")}/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to={pages[0].path} onClick={() => setExpanded(false)}>{pages[0].nav}</Link>
+                <Link className="nav-link" to={pages[2].path} onClick={() => setExpanded(false)}>{pages[2].nav}</Link>
+                <Link className="nav-link" to={pages[1].path} onClick={() => setExpanded(false)}>{pages[1].nav}</Link>
+                <Link className="nav-link" to={pages[3].path} onClick={() => setExpanded(false)}>{pages[3].nav}</Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+
+}
+
+export default Header;
