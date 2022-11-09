a60202d312dcca5d10fafd5b4867fb6a5d583e32
Nile Camai <nile.camai@gmail.com>
2022-11-09 00:26:22 -0800

    ideally fix styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/custom.scss';
-import '../styles/App.css';
-import '../styles/index.css';
 
 import HomePage from './home';
 class App extends React.Component {
